046bacb7b7e96554f8261a80d2edc5d00a2e2a16
Chasteau <davidchasteau@gmail.com>
2017-07-07 18:49:50 +0000

    added Admin Categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,22 +5,24 @@ class Admin::CategoriesController < ApplicationController
   end
 
 
-  # def new
-  #   @category = Category.new
-  # end
+  def new
+    @category = Category.new
+  end
 
-  # def create
-  #   @category = Category.new(category_params)
+  def create
+    @category = Category.new(category_params)
 
-  #   if @category.save
-  #     redirect_to[:admin, :categories], notice 'Category created!'
-  #   else
-  #     render
-  # end
+    if @category.save
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
+  end
 
-  # private
+  private
 
-  # def category_params(:category).permit(:name)
-  # end
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
