f473c99e8cbddbea4f2f96d1319f05f46077f6c0
Chasteau <davidchasteau@gmail.com>
2017-07-11 01:04:43 +0000

    added produce reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,27 @@ class ReviewsController < ApplicationController
 
 
   def create
-    raise "Yay, I'm here"
+    @product_id = params[:product_id]
+    @review = Review.new(review_params)
+    # after @review has been initialized, but before calling .save on it:
+    @review.user_id = current_user.id
+    @review.product_id = @product_id
+
+
+    if @review.save
+      redirect_to product_path(id: @product_id), notice: 'Review created created!'
+    else
+      redirect_to product_path(id: @product_id)
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+    )
   end
 end
